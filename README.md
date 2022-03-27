1c7e6b8e8fba57f8f2a877e410365b6934a04eca
Craig <indigomx9@gmail.com>
2022-03-27 18:15:46 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# CRUD
+
+Initial setup of the application created with TypeScript, React, React Router v6, useContext, and useReducer.
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint mollitia vero excepturi aliquid modi neque minus iusto quae quaerat maiores consectetur alias eius, illum ipsum magnam nobis? Ipsum, fugit et!</p>
+        </React.Fragment>
+    );
+};
+
+
+
